afe7d149853e676f181fd7d438af4e09d9200cfd
miguelmafra <miguelml159357@gmail.com>
2020-06-02 22:35:53 -0300

    Configuração de Request Parm

--- Aulas/Server/src/server.ts
+++ Aulas/Server/src/server.ts
@@ -11,6 +11,7 @@ const app = express();
 //PUT: Atualizar uma informação existente no back-end
 //DELETE: Deletar uma informação do back-end
 
+//request Param : Parametro que vem na própria rota que identificam um recurso
 const users = [
     'Miguel',
     'Lucas',
@@ -18,13 +19,14 @@ const users = [
     'Daniel'
 ];
 
-
+//lista todos os usuários do array
 app.get('/users', (request, response) => {
     console.log('Listagem de Usuários');
 
     return response.json(users);
 });
 
+//lista um usuário especifico conforme o ID procurado
 app.get('/users/:id', (request, response) => {
     const id = Number(request.params.id);
 
@@ -33,6 +35,7 @@ app.get('/users/:id', (request, response) => {
     return response.json(user);
 });
 
+//cria um usuário
 app.post('/users', (request, response) => {
     const user = {
         name: 'Diego',
