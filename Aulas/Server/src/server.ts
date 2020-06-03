8205ad6527393b1bdc0bb439a8a22f6f76427b4c
miguelmafra <miguelml159357@gmail.com>
2020-06-02 22:01:20 -0300

    Criando e testando método do tipo POST

--- Aulas/Server/src/server.ts
+++ Aulas/Server/src/server.ts
@@ -1,6 +1,15 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+
+// Rota: Endereço completo da requisição (exemplo localhost:3333/users)
+// Recurso: Qual entidade estamos acessando do sistema (exemplo /users)
+
+//GET: Buscar uma ou mais informações do back-end
+//POST: Criar uma nova informação no back-end
+//PUT: Atualizar uma informação existente no back-end
+//DELETE: Deletar uma informação do back-end
 
 app.get('/users', (request,response) => {
     console.log('Listagem de Usuários');
@@ -11,6 +20,15 @@ app.get('/users', (request,response) => {
         'Alex',
         'Daniel'
     ]);
+})
+
+app.post('/users', (request,response) => {
+    const user ={
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    }
+
+     return response.json(user);
 })
 
 app.listen(3333);
