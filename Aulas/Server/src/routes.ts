3ea286a655d6efdd2db293cfa830154fe2b568e7
miguelmafra <miguelml159357@gmail.com>
2020-06-03 00:55:46 -0300

    Realização de teste de cadastro

--- Aulas/Server/src/routes.ts
+++ Aulas/Server/src/routes.ts
@@ -7,6 +7,7 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item =>{
         return {
+            id:item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
