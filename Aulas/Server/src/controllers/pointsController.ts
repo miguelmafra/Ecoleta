e5a4d1c45234cdd15f45e595a2a43f8172db80e3
miguelmafra <miguelml159357@gmail.com>
2020-06-03 22:06:11 -0300

    Criação de listagem de pontos filtrados

--- Aulas/Server/src/controllers/pointsController.ts
+++ Aulas/Server/src/controllers/pointsController.ts
@@ -3,6 +3,43 @@ import knex from '../database/connection';
 
 class PointsController{
 
+    async index(request: Request, response: Response){
+        //cidade , uf, items (Query params)
+        const {city, uf, items} = request.query;
+
+        const parsedItems = String(items).split(',')
+        .map(items => Number(items.trim()));
+
+        const points =  await knex('points')
+        .join('point_items', 'points.id', '=', 'point_items.point_id')
+        .whereIn('point_items.point_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('points.*');
+
+        return response.json(points); 
+    }
+ 
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+
+        const point = await knex('points').where('id', id).first();
+
+        if(!point){
+            return response.status(400).json({message: 'Point not Found. '})
+        }
+
+        //SELECT * FROM items JOIN point_items ON items.id = point_items_id 
+        //WHERE point_items.point_id = {id}
+
+        const items = await knex('items')
+        .join('point_items', 'items.id', '=', 'point_items.item_id')
+        .where('point_items.point_id', id)
+        .select('items.title');
+
+        return response.json({point, items});
+    }
     async create (request: Request,response: Response) {
         const {
             name,
--- Aulas/Server/src/routes.ts
+++ Aulas/Server/src/routes.ts
@@ -1,16 +1,23 @@
 import express, { request, response } from 'express';
-import knex from './database/connection';
 import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
 
+//index: listagem
+//show: exibir um único registro
+//create: cadastro
+//update: atualizar
+//delete: deletar
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', );
-
-
-
+routes.get('/items', itemsController.index );
 
 routes.post('/points', pointsController.create);
+routes.get('/points/', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
 
 export default routes;
